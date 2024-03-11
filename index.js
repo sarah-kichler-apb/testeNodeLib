16dd1d7e6173be4f7aed1070f47f3a4f089010ae
Sarah <sarah.kichler.estudante@petrobras.com.br>
2024-03-11 14:47:50 -0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+console.log(chalk.green('Hello world!'));
+
+//desencadear métodos de texto, cor de fundo, negrito
+console.log(chalk.yellow.bold("CAFÉ"));
+//receber múltiplos argumentos
+console.log(chalk.yellow.bgBlue('É ', 'BOM ', 'DEMAIS.'));
+//Métodos aninhados
+console.log(chalk.red("vermelho", chalk.underline.blue('azul')));
+//Uso de templates e strings
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
