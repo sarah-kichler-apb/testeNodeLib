977c65d92f676fad7af2b5c3750d894aa3b218a1
Sarah <sarah.kichler.estudante@petrobras.com.br>
2024-03-12 14:48:52 -0300

    Abrir aquivos com fs

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+import fs from "fs"
+
 import chalk from 'chalk';
+/*
 console.log(chalk.green('Hello world!'));
 
 //desencadear métodos de texto, cor de fundo, negrito
-console.log(chalk.yellow.bold("CAFÉ"));
+console.log(chalk.yellow.bold("CAFÉ..."));
+
 //receber múltiplos argumentos
-console.log(chalk.yellow.bgBlue('É ', 'BOM ', 'DEMAIS.'));
+console.log(chalk.yellow.bgBlue('É ', 'BOM ', 'DEMAIS!'));
+
 //Métodos aninhados
-console.log(chalk.red("vermelho", chalk.underline.blue('azul')));
+console.log(chalk.underline.red("vermelho", chalk.underline.blue(' azul')));
+
 //Uso de templates e strings
 console.log(`
 CPU: ${chalk.red('90%')}
 RAM: ${chalk.green('40%')}
 DISK: ${chalk.yellow('70%')}
 `);
+*/
+
+//2) Função para tratar dos erros
+function trataErro(erro){
+    throw new Error(chalk.red. underline(erro.code, 'Não há arquivo no diretório. Por favor, verifique o caminho e o diretório novamente.'));
+}
+
+//1) Escrever uma function que traga o conteúdo de arquivos .md
+function pegaConteudo(caminhoDoArquivo){
+    const encoding = "utf-8";
+    fs.readFile(caminhoDoArquivo,encoding,(erro,texto)=>{
+        if(erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaConteudo("./arquivos/texto.md");
+// Callback (erro, texto)
