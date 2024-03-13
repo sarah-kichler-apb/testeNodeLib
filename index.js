cb6889310d0ed87f2954d96e69b1c80395bd0186
Sarah <sarah.kichler.estudante@petrobras.com.br>
2024-03-13 15:26:57 -0300

    Tornar função assíncrona

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ console.log(`
 CPU: ${chalk.red('90%')}
 RAM: ${chalk.green('40%')}
 DISK: ${chalk.yellow('70%')}
-`);
-*/
+`);*/
 
 //2) Função para tratar dos erros
-function trataErro(erro){
-    throw new Error(chalk.red. underline(erro.code, 'Não há arquivo no diretório. Por favor, verifique o caminho e o diretório novamente.'));
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red.underline(erro.code, 'Não há arquivo no diretório. Por favor, verifique o caminho e o diretório novamente.'));
 }
-
+/*
 //1) Escrever uma function que traga o conteúdo de arquivos .md
 function pegaConteudo(caminhoDoArquivo){
     const encoding = "utf-8";
@@ -39,3 +39,29 @@ function pegaConteudo(caminhoDoArquivo){
 
 pegaConteudo("./arquivos/texto.md");
 // Callback (erro, texto)
+
+//***************************************Aula sobrepromessas - inserir método assíncrono no código************************************************
+//1) reescrevendo (refatorando)
+function pegaConteudo(caminhoDoArquivo){
+    const encoding = 'utf-8';
+    fs.promises.readFile(caminhoDoArquivo,encoding).then((texto)=>
+        console.log(chalk.yellow(texto))).catch((erro)=>trataErro(erro));
+}
+
+pegaConteudo('./arquivos/texto.md');*/
+
+//Usando outra forma de solucionar as promessas (async/await)
+async function pegaConteudo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.magenta(texto));
+
+    } catch(erro){
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('Operação concluída'));
+    }
+}
+pegaConteudo('./arquivos/texto.md');
+pegaConteudo('./arquivos/teixto.md');
