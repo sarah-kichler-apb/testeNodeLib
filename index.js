29a966b5a3a6f150f5714ca9fdee54d2e05b5255
Sarah <sarah.kichler.estudante@petrobras.com.br>
2024-03-15 15:15:01 -0300

    Alterando função extraLinks e transformando em vetor

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import fs from "fs"
 
 import chalk from 'chalk';
 
-const regex = '\[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)'
+
 const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
-function extraiLinks (texto){
-    const regex = /\[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)/gm
-    const capturas = texto.match(regex);
-    console.log(capturas);
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
+    //const capturas = texto.match(regex);
+    //const capturas = regex.exec(texto);
+    //const capturas = texto.matchAll(regex);
+
+    //Operador de espalhamento
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura=>({
+        [captura[1]]:captura[2]
+    }))
+
+
+    console.log(resultados);
 }
 
 extraiLinks(textoTeste);
